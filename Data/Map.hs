12fe547c13266e2b7f48a408451eaa13519785b8
Ian Lynagh <igloo@earth.li>
2009-11-29 15:45:19 +0000

    Tweak layout to work with the alternative layout rule

--- Data/Map.hs
+++ Data/Map.hs
@@ -1278,7 +1278,7 @@ mapEitherWithKey _ Tip = (Tip, Tip)
 mapEitherWithKey f (Bin _ kx x l r) = case f kx x of
   Left y  -> (join kx y l1 r1, merge l2 r2)
   Right z -> (merge l1 r1, join kx z l2 r2)
-  where
+ where
     (l1,l2) = mapEitherWithKey f l
     (r1,r2) = mapEitherWithKey f r
 
