471d6b5b66b290bed0d917f340c578d443a9803c
Joachim Breitner <mail@joachim-breitner.de>
2011-09-18 17:14:23 +0200

    bitcount source reference

--- Data/DenseIntSet.hs
+++ Data/DenseIntSet.hs
@@ -1199,7 +1199,15 @@ highestBitSet n0 =
 ----------------------------------------------------------------------}
 
 {----------------------------------------------------------------------
-  [bitcount] as posted by David F. Place to haskell-cafe on April 11, 2006
+  [bitcount] as posted by David F. Place to haskell-cafe on April 11, 2006,
+  based on the code on
+  http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetKernighan,
+  where the following source is given:
+    Published in 1988, the C Programming Language 2nd Ed. (by Brian W.
+    Kernighan and Dennis M. Ritchie) mentions this in exercise 2-9. On April
+    19, 2006 Don Knuth pointed out to me that this method "was first published
+    by Peter Wegner in CACM 3 (1960), 322. (Also discovered independently by
+    Derrick Lehmer and published in 1964 in a book edited by Beckenbach.)"
 ----------------------------------------------------------------------}
 bitcount :: Int -> Word -> Int
 bitcount a 0 = a
