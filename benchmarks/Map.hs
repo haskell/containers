cfb472c87df53c37c7b54561d0b727ffbc772be0
Milan Straka <fox@ucw.cz>
2012-08-30 16:51:57 +0200

    Add fromList variants to benchmark.
    Also set the size of inputs for various containers to the same value 2^12.

--- benchmarks/Map.hs
+++ benchmarks/Map.hs
@@ -58,9 +58,12 @@ main = do
         , bench "difference" $ whnf (M.difference m) m_even
         , bench "intersection" $ whnf (M.intersection m) m_even
         , bench "split" $ whnf (M.split (bound `div` 2)) m
+        , bench "fromList" $ whnf M.fromList elems
+        , bench "fromAscList" $ whnf M.fromAscList elems
+        , bench "fromDistinctAscList" $ whnf M.fromAscList elems
         ]
   where
-    bound = 2^10
+    bound = 2^12
     elems = zip keys values
     elems_even = zip evens evens
     elems_odd = zip odds odds
