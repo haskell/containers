172891866206824475842796976e57ead22c26a9
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-04-30 12:55:53 +0100

    Benchmarks include stable sort

--- benchmarks/SequenceSorting.hs
+++ benchmarks/SequenceSorting.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import           Criterion.Main (Benchmark, bench, defaultMain, env, nf, bgroup)
+import           Criterion.Main (Benchmark, bench, bgroup, defaultMain, env, nf)
 import           System.Random  (randomIO)
 
 import qualified Data.Sequence  as S
@@ -14,19 +14,20 @@ benchAtSize n =
     \xs ->
          bgroup
              (show n)
-             [ bench "old" $ nf S.unstableSort xs ]
+             [bench "to/from list" $ nf S.sort xs
+             ,bench "unstable heapsort" $ nf S.unstableSort xs ]
 
 incBenchAtSize :: Int -> Benchmark
 incBenchAtSize n =
     env (S.unstableSort <$> S.replicateA n randInt) $
     \xs ->
-         bgroup
-             (show n)
-             [ bench "old" $ nf S.unstableSort xs ]
+         bgroup (show n)
+           [ bench "to/from list" $ nf S.sort xs
+           , bench "unstable heapsort" $ nf S.unstableSort xs]
 
 main :: IO ()
 main =
     defaultMain
-        [ bgroup "unordered" $ map benchAtSize [500, 5000, 50000]
-        , bgroup "increasing" $ map incBenchAtSize [500, 5000, 50000]
+        [ bgroup "unordered" $ map benchAtSize [50000]
+        , bgroup "increasing" $ map incBenchAtSize [50000]
         ]
