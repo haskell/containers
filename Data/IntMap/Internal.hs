e19a5567b93a71fb6cb2bda54a52d9bbe6d08e22
Matt Renaud <matt@m-renaud.com>
2018-01-08 11:38:48 -0800

    INLINABLE, not INLINEABLE.
    Both spellings work but the docs use "INLINABLE".
    
    [ci skip]

--- Data/IntMap/Internal.hs
+++ Data/IntMap/Internal.hs
@@ -995,7 +995,7 @@ alterF f k m = (<$> f mv) $ \fres ->
     Nothing -> maybe m (const (delete k m)) mv
     Just v' -> insert k v' m
   where mv = lookup k m
-{-# INLINEABLE [1] alterF #-}
+{-# INLINABLE [1] alterF #-}
 #if MIN_VERSION_base(4,8,0)
 {-# RULES
 "Identity specialize alterF" forall (f :: Maybe a -> Identity (Maybe a)) k m.
