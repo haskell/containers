580556febc99ae67ae7b7fa80480e42dd4b04ebe
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-23 23:42:47 +0000

    removed coersion that was causing trouble on GHC 7.8. (it's not being used anyway)

--- Utils/Containers/Internal/Coercions.hs
+++ Utils/Containers/Internal/Coercions.hs
@@ -9,17 +9,12 @@ module Utils.Containers.Internal.Coercions where
 import Data.Coerce
 #endif
 
-infixr 9 .#, #.
+infixr 9 .#
 #if __GLASGOW_HASKELL__ >= 708
 (.#) :: Coercible b a => (b -> c) -> (a -> b) -> a -> c
 (.#) f _ = coerce f
-(#.) :: Coercible c b => (b -> c) -> (a -> b) -> a -> c
-(#.) _ = coerce
 #else
 (.#) :: (b -> c) -> (a -> b) -> a -> c
 (.#) = (.)
-(#.) :: (b -> c) -> (a -> b) -> a -> c
-(#.) = (.)
 #endif
 {-# INLINE (.#) #-}
-{-# INLINE (#.) #-}
