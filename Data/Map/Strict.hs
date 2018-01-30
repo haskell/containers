71f1668418d8658a95535597150af398a87b0229
Matt Renaud <matt@m-renaud.com>
2018-01-30 12:20:17 -0800

    map -> maps
    [ci skip]

--- Data/Map/Strict.hs
+++ Data/Map/Strict.hs
@@ -71,7 +71,7 @@
 -- The 'Map' type is shared between the lazy and strict modules, meaning that
 -- the same 'Map' value can be passed to functions in both modules. This means
 -- that the 'Functor', 'Traversable' and 'Data' instances are the same as for
--- the "Data.Map.Lazy" module, so if they are used the resulting map may contain
+-- the "Data.Map.Lazy" module, so if they are used the resulting maps may contain
 -- suspended values (thunks).
 --
 --
