ed194f17a19535f6634fffbc07e581c6ad8f01ac
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2018-10-16 16:06:02 +0900

    Fix sample code of alterF in haddock
    Found by a question in StackOverflow https://stackoverflow.com/questions/52828746/haskell-data-map-strict-alterf-parse-error-on-input/52829097

--- Data/Map/Internal.hs
+++ Data/Map/Internal.hs
@@ -1167,14 +1167,14 @@ data AreWeStrict = Strict | Lazy
 -- @
 -- interactiveAlter :: Int -> Map Int String -> IO (Map Int String)
 -- interactiveAlter k m = alterF f k m where
---   f Nothing -> do
+--   f Nothing = do
 --      putStrLn $ show k ++
 --          " was not found in the map. Would you like to add it?"
 --      getUserResponse1 :: IO (Maybe String)
---   f (Just old) -> do
---      putStrLn "The key is currently bound to " ++ show old ++
+--   f (Just old) = do
+--      putStrLn $ "The key is currently bound to " ++ show old ++
 --          ". Would you like to change or delete it?"
---      getUserresponse2 :: IO (Maybe String)
+--      getUserResponse2 :: IO (Maybe String)
 -- @
 --
 -- 'alterF' is the most general operation for working with an individual
