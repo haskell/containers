559fac339e3632c990431e126aa090bdcbdf5ddb
Boro Sitnikovski <buritomath@gmail.com>
2018-11-28 03:37:25 +0100

    Rephrase sentence

--- Data/IntMap/Internal.hs
+++ Data/IntMap/Internal.hs
@@ -32,7 +32,7 @@
 --
 -- The Package Versioning Policy __does not apply__.
 --
--- This contents of this module may change __in any way whatsoever__
+-- The contents of this module may change __in any way whatsoever__
 -- and __without any warning__ between minor versions of this package.
 --
 -- Authors importing this module are expected to track development
