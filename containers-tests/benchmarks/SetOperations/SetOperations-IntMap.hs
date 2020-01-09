3a34c0980f971272e8294809dfc91d3918514cd5
Jonathan S <gereeter+code@gmail.com>
2020-01-08 18:10:32 -0600

    Don't bind arguments in IntMap.intersectionWith. This may have been partially justified before, but is costly now.
    ```
    Benchmark                        Runtime change  Original runtime
    intersectionWith-block_nn         -20.76%        1.61e-05
    intersectionWith-block_nn_swap    -23.81%        1.61e-05
    intersectionWith-block_ns         -30.41%        2.04e-06
    intersectionWith-block_sn_swap    -31.58%        2.13e-06
    intersectionWith-common_nn         -4.51%        1.02e-02
    intersectionWith-common_nn_swap    -4.03%        1.02e-02
    intersectionWith-common_ns         -0.07%        1.26e-03
    intersectionWith-common_nt        -16.17%        3.26e-05
    intersectionWith-common_sn_swap    +0.35%        1.28e-03
    intersectionWith-common_tn_swap   -13.89%        3.22e-05
    intersectionWith-disj_nn          -79.22%        1.21e-07
    intersectionWith-disj_nn_swap     -78.40%        1.19e-07
    intersectionWith-disj_ns          -79.31%        1.21e-07
    intersectionWith-disj_nt          -79.25%        1.21e-07
    intersectionWith-disj_sn_swap     -78.44%        1.19e-07
    intersectionWith-disj_tn_swap     -78.49%        1.19e-07
    intersectionWith-mix_nn           +11.27%        2.59e-03
    intersectionWith-mix_nn_swap      +11.45%        2.58e-03
    intersectionWith-mix_ns            -3.69%        3.56e-04
    intersectionWith-mix_nt           -17.32%        2.59e-05
    intersectionWith-mix_sn_swap       -4.98%        3.70e-04
    intersectionWith-mix_tn_swap      -11.32%        2.43e-05
    
    Minimum                           -79.31%
    Average                           -39.80%
    Maximum                           +11.45%
    ```

--- containers-tests/benchmarks/SetOperations/SetOperations-IntMap.hs
+++ containers-tests/benchmarks/SetOperations/SetOperations-IntMap.hs
@@ -3,4 +3,8 @@ module Main where
 import Data.IntMap as C
 import SetOperations
 
-main = benchmark (\xs -> fromList [(x, x) | x <- xs]) True [("union", C.union), ("unionWith", C.unionWith (+)), ("difference", C.difference), ("intersection", C.intersection)]
+main = benchmark (\xs -> fromList [(x, x) | x <- xs]) True
+    [ ("union", C.union), ("unionWith", C.unionWith (+))
+    , ("difference", C.difference)
+    , ("intersection", C.intersection), ("intersectionWith", C.intersectionWith (+))
+    ]
