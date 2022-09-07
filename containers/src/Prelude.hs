5251866d5f45c09487e0e8298f5f065f90f81714
Georgi Lyubenov <georgi.lyubenov@tweag.io>
2022-09-07 15:27:16 +0300

    Use less cpp and don't rely on base version for liftA2 comaptibility

--- containers/src/Prelude.hs
+++ containers/src/Prelude.hs
@@ -4,19 +4,15 @@
 -- @liftA2@ wasn't previously exported from the standard prelude.
 module Prelude
   ( module Prel
-#if !MIN_VERSION_base(4,18,0)
   , Applicative (..)
-#endif
 #if !MIN_VERSION_base(4,10,0)
   , liftA2
 #endif
   )
   where
 
-import "base" Prelude as Prel
-#if !MIN_VERSION_base(4,18,0)
+import "base" Prelude as Prel hiding (Applicative(..))
 import Control.Applicative(Applicative(..))
-#endif
 
 #if !MIN_VERSION_base(4,10,0)
 import Control.Applicative(liftA2)
