4ed4883369c2d0de909c34a5695511027663d48c
meooow25 <soumiksarkar.3120@gmail.com>
2022-12-20 01:39:58 +0530

    Benchmark with more random graphs

--- containers-tests/benchmarks/Graph.hs
+++ containers-tests/benchmarks/Graph.hs
@@ -1,40 +1,55 @@
 module Main where
 
-import Control.DeepSeq (rnf)
+import Control.DeepSeq (NFData, rnf)
 import Control.Exception (evaluate)
 import Data.Array (assocs, bounds)
 import System.Random (mkStdGen, randomRs)
-import Test.Tasty.Bench (bench, defaultMain, nf)
+import Test.Tasty.Bench (Benchmark, Benchmarkable, bench, bgroup, defaultMain, nf)
 import qualified Data.Graph as G
 
 main :: IO ()
 main = do
-  evaluate $ rnf randomG `seq` rnf randomGEdges `seq` rnf randomGAdjList
+  evaluate $ rnf randGs
   defaultMain
-    [ bench "buildG" $ nf (G.buildG (bounds randomG)) randomGEdges
-    , bench "graphFromEdges" $ nf ((\(g, _, _) -> g) . G.graphFromEdges) randomGAdjList
-    , bench "dfs" $ nf (flip G.dfs [1]) randomG
-    , bench "dff" $ nf G.dff randomG
-    , bench "topSort" $ nf G.topSort randomG
-    , bench "scc" $ nf G.topSort randomG
-    , bench "bcc_small" $ nf G.bcc smallRandomG
-    , bench "stronglyConnCompR" $ nf G.stronglyConnCompR randomGAdjList
+    [ bgroup "buildG" $ forGs randGs $ \g -> nf (G.buildG (bounds (getG g))) (getEdges g)
+    , bgroup "graphFromEdges" $ forGs randGs $ nf ((\(g, _, _) -> g) . G.graphFromEdges) . getAdjList
+    , bgroup "dfs" $ forGs randGs $ nf (flip G.dfs [1]) . getG
+    , bgroup "dff" $ forGs randGs $ nf G.dff . getG
+    , bgroup "topSort" $ forGs randGs $ nf G.topSort . getG
+    , bgroup "scc" $ forGs randGs $ nf G.scc . getG
+    , bgroup "bcc" $ forGs [randG1, randG2] $ nf G.bcc . getG
+    , bgroup "stronglyConnCompR" $ forGs randGs $ nf G.stronglyConnCompR . getAdjList
     ]
   where
-    randomG = buildRandomGraph 10000 100000
-    randomGEdges = G.edges randomG
-    randomGAdjList = [(u, u, vs) | (u, vs) <- assocs randomG]
-    smallRandomG = buildRandomGraph 1000 10000
+    randG1 = buildRandG 100 1000 
+    randG2 = buildRandG 100 10000
+    randG3 = buildRandG 10000 100000
+    randGs = [randG1, randG2, randG3]
 
 -- Note: In practice it does not make sense to run topSort or bcc on a random
 -- graph. For topSort the graph should be acyclic and for bcc the graph should
 -- be undirected. But these functions don't check or depend on these properties,
 -- so we can keep things simple and run them on random graphs in benchmarks.
 
+forGs :: [Graph] -> (Graph -> Benchmarkable) -> [Benchmark]
+forGs gs f = [bench (getLabel g) (f g) | g <- gs]
+
+data Graph = Graph
+  { getLabel :: String
+  , getG :: G.Graph
+  , getEdges :: [(G.Vertex, G.Vertex)]
+  , getAdjList :: [(Int, G.Vertex, [G.Vertex])]
+  }
+
+instance NFData Graph where
+  rnf (Graph label g edges adj) = rnf label `seq` rnf g `seq` rnf edges `seq` rnf adj
+
 -- A graph with vertices [1..n] and m random edges.
-buildRandomGraph :: Int -> Int -> G.Graph
-buildRandomGraph n m = G.buildG (1, n) (zip us vs)
+buildRandG :: Int -> Int -> Graph
+buildRandG n m = Graph label g (G.edges g) [(u, u, vs') | (u, vs') <- assocs g]
   where
+    label = "n=" ++ show n ++ ",m=" ++ show m
     xs = randomRs (1, n) (mkStdGen 1)
     (us, xs') = splitAt m xs
     vs = take m xs'
+    g = G.buildG (1, n) (zip us vs)
