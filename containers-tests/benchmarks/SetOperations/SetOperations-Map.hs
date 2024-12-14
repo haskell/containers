b0b2cb5100bfca531f59e091fc3d562861662195
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2024-12-14 18:59:59 +0530

    Use randomly structured Set and Map in benchmarks (#1075)
    Trees constructed from [1..n] have a specific structure of perfect
    binary trees linked together. We shuffle the list so that the tree
    is generated from repeated insertions instead, which forms a random
    structure that should be more representative of average use cases.
    
    Most benchmarks show an increase in measured times, the amount varying
    from 10% to 70%. This is expected because the trees are more unbalanced
    now. A handful of benchmarks show larger increases. These were previously
    measuring best-case scenarios, which no longer occur with random trees.

--- containers-tests/benchmarks/SetOperations/SetOperations-Map.hs
+++ containers-tests/benchmarks/SetOperations/SetOperations-Map.hs
@@ -3,8 +3,10 @@ module Main where
 import Data.Map as C
 import SetOperations
 
+import Utils.Random (shuffle)
+
 main :: IO ()
-main = benchmark (\xs -> fromList [(x, x) | x <- xs]) True
+main = benchmark (\xs -> fromList [(x, x) | x <- shuffle xs]) True
   [ ("union", C.union)
   , ("difference", C.difference)
   , ("intersection", C.intersection)
