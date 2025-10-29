fc3bb524b956bc7110b233d4e2ad8c874f9fa6fa
L0neGamer <benjaminbwm@gmail.com>
2025-10-29 23:40:27 +0000

    add benchmark script

---
+++ bench.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+exitWith () {
+  echo "$1"
+  exit $(($2))
+}
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "there are changes, exiting benchmark script";
+  exit 1
+fi
+
+CURR=`git rev-parse --abbrev-ref HEAD`
+
+if [ "$CURR" == "master" ]
+then
+  exitWith "current branch is master, ending benchmarking" -1
+fi
+
+BENCHMARKS=(
+  intmap-benchmarks
+  intset-benchmarks
+  map-benchmarks
+  tree-benchmarks
+  sequence-benchmarks
+  set-benchmarks
+  graph-benchmarks
+  set-operations-intmap
+  set-operations-intset
+  set-operations-map
+  set-operations-set
+  lookupge-intmap
+  lookupge-map
+)
+
+BENCHMARK_TMP="benchmark_tmp"
+
+mkdir -p $BENCHMARK_TMP
+
+git checkout master
+
+cabal build all || exitWith "master build errored" 2
+
+MASTER_BENCH_LOG="$BENCHMARK_TMP/bench-master.log"
+echo -n > $MASTER_BENCH_LOG
+
+for BENCHMARK in "${BENCHMARKS[@]}"
+do
+  echo "running $BENCHMARK on master"
+  (cabal bench $BENCHMARK --benchmark-options="--csv $BENCHMARK.csv" >> $MASTER_BENCH_LOG 2>&1) ||
+    exitWith "benchmark $BENCHMARK failed to run on master, exiting" 3
+done
+
+git checkout $CURR
+
+cabal build all || exitWith "$CURR build errored" 4
+
+CURR_BENCH_LOG="$BENCHMARK_TMP/bench-$CURR.log"
+echo -n > $CURR_BENCH_LOG
+
+for BENCHMARK in "${BENCHMARKS[@]}"
+do
+  echo "running $BENCHMARK on $CURR"
+  (cabal bench $BENCHMARK --benchmark-options="--csv $BENCHMARK-$CURR.csv --baseline $BENCHMARK.csv" >> $CURR_BENCH_LOG 2>&1) ||
+    exitWith "benchmark $BENCHMARK failed to run on $CURR, exiting" 5
+done
+
+mv containers-tests/*.csv $BENCHMARK_TMP/
