722218d1a67dca330b99eeed10d9dbd782ff7b5f
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2026-08-16 15:35:54 +0200

    Add an API to merge a Set and Map into a Map (#1227)
    This is intended to serve some use cases more efficiently than existing
    methods, such as partitioning a map according to whether the keys exist
    in a set.
    
    Similar to Map merging, there are two modules, "Data.Map.Merge.Set.Lazy"
    and "Data.Map.Merge.Set.Strict" exposing the merge functions and
    tactics. The WhenMissing tactic for missing maps is simply reused from
    the Map merging interface. A new WhenMatched tactic is added. A new
    WhenMissingSet tactic is also added, and I chose to added the "Set"
    suffix to distinguish it from the existing map tactic. Similarly,
    the functions that create WhenMissingSet tactics are suffixed with
    "MissingSet".

---
+++ containers/src/Data/Map/Merge/Set/Internal.hs
@@ -0,0 +1,254 @@
+{-# OPTIONS_HADDOCK not-home #-}
+
+-- |
+-- = WARNING
+--
+-- This module is considered __internal__.
+--
+-- The Package Versioning Policy __does not apply__.
+--
+-- The contents of this module may change __in any way whatsoever__
+-- and __without any warning__ between minor versions of this package.
+--
+-- Authors importing this module are expected to track development
+-- closely.
+--
+-- = Description
+--
+-- This module defines common constructs used by both "Data.Map.Merge.Set.Lazy"
+-- and "Data.Map.Merge.Set.Strict".
+--
+-- @since FIXME
+--
+module Data.Map.Merge.Set.Internal
+  ( WhenMatched(..)
+  , SimpleWhenMatched
+  , filterMatched
+  , filterAMatched
+
+  , WhenMissingSet(..)
+  , SimpleWhenMissingSet
+  , dropMissingSet
+
+  , merge
+  , mergeA
+
+  , runWhenMatched
+  , runWhenMissingSet
+  ) where
+
+import Control.Applicative (liftA3)
+import Data.Functor.Identity (Identity(..))
+import Data.Set (Set)
+import qualified Data.Set.Internal as S
+import Data.Map (Map)
+import qualified Data.Map.Internal as M
+
+-- | A tactic for dealing with keys present in both the set and the map in
+-- 'merge' or 'mergeA'.
+--
+-- A tactic of type @WhenMatched f k a b@ is an abstract representation of
+-- a function of type @k -> a -> f (Maybe b)@.
+--
+-- @since FIXME
+newtype WhenMatched f k a b = WhenMatched
+  { matchedKey :: k -> a -> f (Maybe b)
+  }
+
+-- | Run @WhenMatched@.
+--
+-- @since FIXME
+runWhenMatched :: WhenMatched f k a b -> k -> a -> f (Maybe b)
+runWhenMatched = matchedKey
+
+-- | A tactic for dealing with keys present in both the set and the map in
+-- 'merge'.
+--
+-- A tactic of type @SimpleWhenMatched k a b@ is an abstract representation of
+-- a function of type @k -> a -> Maybe b@.
+--
+-- @since FIXME
+type SimpleWhenMatched = WhenMatched Identity
+
+-- | When a key is found in both the map and the set, apply a function to the
+-- key and the value in the map and keep the value in the merged map if the
+-- result is @True@.
+--
+-- @since FIXME
+filterMatched :: Applicative f => (k -> a -> Bool) -> WhenMatched f k a a
+filterMatched f =
+  WhenMatched (\k x -> if f k x then pure (Just x) else pure Nothing)
+{-# INLINE filterMatched #-}
+
+-- | When a key is found in both the map and the set, apply a function to the
+-- key and the value in the map and keep the value in the merged map if the
+-- result of the action is @True@.
+--
+-- @since FIXME
+filterAMatched :: Functor f => (k -> a -> f Bool) -> WhenMatched f k a a
+filterAMatched f =
+  WhenMatched (\k x -> (\b -> if b then Just x else Nothing) <$> f k x)
+{-# INLINE filterAMatched #-}
+
+-- | A tactic for dealing with keys present in the set but not in the map in
+-- 'merge' or 'mergeA'.
+--
+-- A tactic of type @WhenMissingSet f k a@ is an abstract representation of
+-- a function of type @k -> f (Maybe a)@.
+--
+-- @since FIXME
+data WhenMissingSet f k a = WhenMissingSet
+  { missingSubtree :: Set k -> f (Map k a)
+  , missingKey :: k -> f (Maybe a)
+  }
+
+-- | Run @WhenMissingSet@.
+--
+-- @since FIXME
+runWhenMissingSet :: WhenMissingSet f k a -> k -> f (Maybe a)
+runWhenMissingSet = missingKey
+
+-- | A tactic for dealing with keys present in the set but not in the map in
+-- 'merge'.
+--
+-- A tactic of type @SimpleWhenMissingSet k a@ is an abstract representation of
+-- a function of type @k -> Maybe a@.
+--
+-- @since FIXME
+type SimpleWhenMissingSet = WhenMissingSet Identity
+
+-- | Drop keys that are present in the set but missing from the map.
+--
+-- @since FIXME
+dropMissingSet :: Applicative f => WhenMissingSet f k a
+dropMissingSet = WhenMissingSet
+  { missingSubtree = \_ -> pure M.empty
+  , missingKey = \_ -> pure Nothing
+  }
+{-# INLINE dropMissingSet #-}
+
+-- | Merge a map and a set into a map.
+--
+-- 'merge' takes a 'M.SimpleWhenMissing' tactic, a 'SimpleWhenMissingSet'
+-- tactic, a 'SimpleWhenMatched' tactic, a map and a set. It uses the tactics to
+-- merge the map and the set into a map.
+--
+-- Its behavior is best understood via the tactics @mapMaybeMissing@,
+-- @generateMaybeMissingSet@, and @mapMaybeMatched@. Consider
+--
+-- @
+-- merge (mapMaybeMissing g1) (generateMaybeMissingSet g2) (mapMaybeMatched f) m1 s2
+-- @
+--
+-- @
+-- g1 k x = if k == 2 then Just ("1" ++ x) else Nothing
+-- g2 k = if k == 3 then Just "2" else Nothing
+-- f k x = if k == 6 then Just ("3" ++ x) else Nothing
+-- m1 = fromList [(2,"a"), (4,"b"), (6,"c"), (8,"d"), (10,"e"), (12,"f")]
+-- s2 = fromList [3, 6, 9, 12]
+-- @
+--
+-- 'merge' will pass the keys and values to @g1@, @g2@, or @f@ as appropriate,
+-- producing a @Maybe@ for each element.
+--
+-- @
+-- m1:      [ (2, "a"),           (4, "b"),  (6, "c"), (8, "d"),          (10, "e"), (12, "f")]
+-- s2:      [                  3,                   6,                 9,                   12]
+-- result:  [ g1 2 "a",     g2 3, g1 4 "b",   f 6 "c", g1 8 "d",    g2 9, g1 10 "e",  f 12 "f"]
+--        = [Just "1a", Just "2",  Nothing, Just "3c",  Nothing, Nothing,   Nothing,   Nothing]
+-- @
+--
+-- The result map contains the @Just@ values.
+--
+-- >>> merge (mapMaybeMissing g1) (generateMaybeMissingSet g2) (mapMaybeMatched f) m1 s2
+-- fromList [(2,"1a"), (3,"2g"), (6,"3c")]
+--
+-- When 'merge' is given three arguments, it is inlined at the call
+-- site. To prevent excessive inlining, you should typically use 'merge'
+-- to define your custom combining functions.
+--
+-- @since FIXME
+merge
+  :: Ord k
+  => M.SimpleWhenMissing k a b -- ^ What to do with keys in @m1@ but not @s2@
+  -> SimpleWhenMissingSet k b -- ^ What to do with keys in @s2@ but not @m1@
+  -> SimpleWhenMatched k a b -- ^ What to do with keys in both @m1@ and @s2@
+  -> Map k a -- ^ Map @m1@
+  -> Set k -- ^ Set @s2@
+  -> Map k b
+merge miss1 miss2 match = \t1 t2 -> runIdentity (mergeA miss1 miss2 match t1 t2)
+{-# INLINE merge #-}
+
+-- | Merge a map and a set into a map. Applicative version of 'merge'.
+--
+-- 'mergeA' takes a 'M.WhenMissing' tactic, a 'WhenMissingSet' tactic, a
+-- 'WhenMatched' tactic, a map and a set. It uses the tactics to merge the map
+-- and the set into a map.
+--
+-- Behaves just like 'merge' while allowing @Applicative@ effects. Effects are
+-- performed in increasing order of keys.
+--
+-- Consider
+--
+-- @
+-- mergeA (traverseMaybeMissing g1)
+--        (generateMaybeAMissingSet g2)
+--        (traverseMaybeMatched f)
+--        m1
+--        s2
+-- @
+--
+-- @
+-- g1 k x = let z = if k == 2 then Just ("1" ++ x) else Nothing
+--          in z <$ putStrLn ("g1 " ++ show (k, x))
+-- g2 k = let z = if k == 3 then Just "2" else Nothing
+--        in z <$ putStrLn ("g2 " ++ show k)
+-- f k x = let z = if k == 6 then Just ("3" ++ x) else Nothing
+--         in z <$ putStrLn ("f " ++ show (k, x))
+-- m1 = fromList [(2,"a"), (4,"b"), (6,"c"), (8,"d"), (10,"e"), (12,"f")]
+-- m2 = fromList [3, 6, 9, 12]
+-- @
+--
+-- As with 'merge', the result map is @[(2,"1a"), (3,"2"), (6,"3c")]@.
+-- Additionally, @g1@, @g2@, and @f@ perform @IO@ effects, printing in
+-- increasing order of key.
+--
+-- >>> mergeA (traverseMaybeMissing g1) (generateMaybeAMissingSet g2) (traverseMaybeMatched f) m1 s2
+-- g1 (2,"a")
+-- g2 3
+-- g1 (4,"b")
+-- f (6,"c")
+-- g1 (8,"d")
+-- g2 9
+-- g1 (10,"e")
+-- f (12,"f")
+-- fromList [(2,"1a"),(3,"2"),(6,"3c")]
+--
+-- When 'mergeA' is given three arguments, it is inlined at the call
+-- site. To prevent excessive inlining, you should generally only use
+-- 'mergeA' to define custom combining functions.
+--
+-- @since FIXME
+mergeA
+  :: (Applicative f, Ord k)
+  => M.WhenMissing f k a b -- ^ What to do with keys in @m1@ but not @s2@
+  -> WhenMissingSet f k b -- ^ What to do with keys in @s2@ but not @m1@
+  -> WhenMatched f k a b -- ^ What to do with keys in both @m1@ and @s2@
+  -> Map k a -- ^ Map @m1@
+  -> Set k -- ^ Set @s2@
+  -> f (Map k b)
+mergeA
+  M.WhenMissing{M.missingSubtree = g1t, M.missingKey = g1k}
+  WhenMissingSet{missingSubtree = g2t}
+  WhenMatched{matchedKey = f} = go
+  where
+    go t1 S.Tip = g1t t1
+    go M.Tip t2 = g2t t2
+    go (M.Bin _ k1 x1 l1 r1) t2 = case S.splitMember k1 t2 of
+      (l2, found, r2) ->
+        liftA3
+          (\l' mx' r' -> maybe M.link2 (M.link k1) mx' l' r')
+          (go l1 l2)
+          (if found then f k1 x1 else g1k k1 x1)
+          (go r1 r2)
+{-# INLINE mergeA #-}
---
+++ containers/src/Data/Map/Merge/Set/Strict.hs
@@ -0,0 +1,162 @@
+{-# LANGUAGE BangPatterns #-}
+
+-- |
+-- This module defines an API for writing functions that merge a map and a
+-- set into a map. The key functions are 'Data.Map.Merge.Set.Strict.merge' and
+-- 'Data.Map.Merge.Set.Strict.mergeA'. Each of these can be used with several
+-- different \"merge tactics\".
+--
+-- The @merge@ and @mergeA@ functions are shared by the lazy and strict
+-- modules. Only the choice of merge tactics determines strictness.
+-- If you use 'Data.Map.Merge.Set.Strict.mapMissing' from this module
+-- then the results will be forced before they are inserted. If you use
+-- 'Data.Map.Merge.Set.Lazy.mapMissing' from "Data.Map.Merge.Set.Lazy" then they
+-- will not.
+--
+-- @since FIXME
+--
+module Data.Map.Merge.Set.Strict
+  (
+  -- ** Simple merge tactic types
+    MS.SimpleWhenMissing
+  , Internal.SimpleWhenMissingSet
+  , Internal.SimpleWhenMatched
+
+  -- ** General combining function
+  , Internal.merge
+
+  -- *** @WhenMatched@ tactics
+  , Internal.filterMatched
+  , mapMatched
+  , mapMaybeMatched
+
+  -- *** @WhenMissing@ tactics
+  , MS.dropMissing
+  , MS.preserveMissing
+  , MS.mapMissing
+  , MS.filterMissing
+  , MS.mapMaybeMissing
+
+  -- *** @WhenMissingSet@ tactics
+  , Internal.dropMissingSet
+  , generateMissingSet
+  , generateMaybeMissingSet
+
+  -- ** Applicative merge tactic types
+  , MS.WhenMissing
+  , Internal.WhenMissingSet
+  , Internal.WhenMatched
+
+  -- ** General combining function
+  , Internal.mergeA
+
+  -- *** @WhenMatched@ tactics
+  , Internal.filterAMatched
+  , traverseMatched
+  , traverseMaybeMatched
+
+  -- *** @WhenMissing@ tactics
+  , MS.filterAMissing
+  , MS.traverseMissing
+  , MS.traverseMaybeMissing
+
+  -- *** @WhenMissingSet@ tactics
+  , generateAMissingSet
+  , generateMaybeAMissingSet
+
+  -- ** Miscellaneous
+  , Internal.runWhenMatched
+  , Internal.runWhenMissingSet
+  ) where
+
+import qualified Data.Map.Strict.Internal as MS
+import qualified Data.Map.Merge.Set.Internal as Internal
+import Data.Map.Merge.Set.Internal (WhenMatched(..), WhenMissingSet(..))
+
+-- | When the key is found in both the map and the set, apply a function to the
+-- key and the value in the map and use the result as the value for the result
+-- map.
+--
+-- @since FIXME
+mapMatched :: Applicative f => (k -> a -> b) -> WhenMatched f k a b
+mapMatched f = WhenMatched (\k x -> pure (Just $! f k x))
+{-# INLINE mapMatched #-}
+
+-- | When a key is found in both the map and the set, apply a function to the
+-- key and the value in the map and maybe use the result as the value for the
+-- merged map.
+--
+-- @since FIXME
+mapMaybeMatched :: Applicative f => (k -> a -> Maybe b) -> WhenMatched f k a b
+mapMaybeMatched f = WhenMatched (\k x -> pure (forceMaybe (f k x)))
+{-# INLINE mapMaybeMatched #-}
+
+-- | When a key is found in both the map and the set, apply a function to the
+-- key and the value in the map, and use the result of the action as the value
+-- for the merged map.
+--
+-- @since FIXME
+traverseMatched :: Functor f => (k -> a -> f b) -> WhenMatched f k a b
+traverseMatched f = WhenMatched (\k x -> (Just $!) <$> f k x)
+{-# INLINE traverseMatched #-}
+
+-- | When a key is found in both the map and the set, apply a function to the
+-- key and the value in the map, and maybe use the result of the action as the
+-- value for the merged map.
+--
+-- @since FIXME
+traverseMaybeMatched
+  :: Functor f => (k -> a -> f (Maybe b)) -> WhenMatched f k a b
+traverseMaybeMatched f = WhenMatched (\k x -> forceMaybe <$> f k x)
+{-# INLINE traverseMaybeMatched #-}
+
+-- | For keys that are present in the set but missing from the map, apply a
+-- function and use the result as the value for the merge map.
+--
+-- @since FIXME
+generateMissingSet :: Applicative f => (k -> a) -> WhenMissingSet f k a
+generateMissingSet f = WhenMissingSet
+  { missingSubtree = \s -> pure (MS.fromSet f s)
+  , missingKey = \k -> pure (Just $! f k)
+  }
+{-# INLINE generateMissingSet #-}
+
+-- | For keys that are present in the set but missing from the map, apply a
+-- function, and use the result of the action as the value for the merged map.
+--
+-- @since FIXME
+generateAMissingSet :: Applicative f => (k -> f a) -> WhenMissingSet f k a
+generateAMissingSet f = WhenMissingSet
+  { missingSubtree = MS.fromSetA f
+  , missingKey = \k -> (Just $!) <$> f k
+  }
+{-# INLINE generateAMissingSet #-}
+
+-- | For keys that are present in the set but missing from the map, apply a
+-- function and maybe use the result as the value for the merged map.
+--
+-- @since FIXME
+generateMaybeMissingSet
+  :: Applicative f => (k -> Maybe a) -> WhenMissingSet f k a
+generateMaybeMissingSet f = WhenMissingSet
+  { missingSubtree = \s -> pure (MS.fromSetMaybe f s)
+  , missingKey = \k -> pure (f k)
+  }
+{-# INLINE generateMaybeMissingSet #-}
+
+-- | For keys that are present in the set but missing from the map, apply a
+-- function, and maybe use the result of the action as the value for the merged
+-- map.
+--
+-- @since FIXME
+generateMaybeAMissingSet
+  :: Applicative f => (k -> f (Maybe a)) -> WhenMissingSet f k a
+generateMaybeAMissingSet f = WhenMissingSet
+  { missingSubtree = MS.fromSetMaybeA f
+  , missingKey = \k -> forceMaybe <$> f k
+  }
+{-# INLINE generateMaybeAMissingSet #-}
+
+forceMaybe :: Maybe a -> Maybe a
+forceMaybe Nothing = Nothing
+forceMaybe m@(Just !_) = m
